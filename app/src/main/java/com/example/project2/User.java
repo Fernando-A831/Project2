fa3b63a340e0ab13f3fbe3a6e4516314f9cfe14a
kharchandani19 <kanishkaharchandani19@gmail.com>
2025-11-28 23:51:44 -0800

    Added User.java and UserDao.java for login assignment

---
+++ app/src/main/java/com/example/project2/User.java
@@ -0,0 +1,26 @@
+package com.example.project2;
+
+import androidx.annotation.NonNull;
+import androidx.room.Entity;
+import androidx.room.PrimaryKey;
+
+@Entity(tableName = "users")
+public class User {
+
+    @PrimaryKey(autoGenerate = true)
+    public int id;
+
+    @NonNull
+    public String username;
+
+    @NonNull
+    public String password;
+
+    public boolean isAdmin;
+
+    public User(@NonNull String username, @NonNull String password, boolean isAdmin) {
+        this.username = username;
+        this.password = password;
+        this.isAdmin = isAdmin;
+    }
+}
